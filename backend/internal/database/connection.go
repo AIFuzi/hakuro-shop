198c135077f7917b36fbc7eb0a0bc485b2f2dcbb
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2024-04-26 05:56:21 +0300

    Connect to prosgtres db

---
+++ backend/internal/database/connection.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"github.com/AIFuzi/hakuro-shop/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+var DB *gorm.DB
+
+func New() {
+	conn, err := gorm.Open(postgres.Open(os.Getenv("DB_URL")))
+	if err != nil {
+		log.Fatal("Fatal error: Failed to connect database")
+	}
+
+	DB = conn
+
+	err = conn.AutoMigrate(models.User{}, models.Types{})
+	if err != nil {
+		log.Fatal("Fatal error: Failed to migrate models")
+	}
+}
