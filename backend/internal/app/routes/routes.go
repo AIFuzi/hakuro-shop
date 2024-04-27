5b83018eb55b8d7dcd239dc8ab2aefd156b34565
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2024-04-27 08:42:54 +0300

    Routes for user api

--- backend/internal/app/routes/routes.go
+++ backend/internal/app/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/AIFuzi/hakuro-shop/internal/app/controllers/product-controller"
 	"github.com/AIFuzi/hakuro-shop/internal/app/controllers/type-controller"
+	user_controller "github.com/AIFuzi/hakuro-shop/internal/app/controllers/user-controller"
 	"github.com/AIFuzi/hakuro-shop/internal/app/middleware"
 	"github.com/gofiber/fiber/v2"
 )
@@ -20,4 +21,9 @@ func Setup(app *fiber.App) {
 	app.Get("/api/product:id", product_controller.GetOne)
 	app.Post("/api/product/create", middleware.CheckIsAdmin, product_controller.Create)
 	app.Delete("/api/product/delete/:id", middleware.CheckIsAdmin, product_controller.Delete)
+
+	app.Post("api/user/signup", user_controller.Register)
+	app.Post("api/user/login", user_controller.Login)
+	app.Post("api/user/logout", user_controller.Logout)
+	app.Get("api/user/check", user_controller.Check)
 }
