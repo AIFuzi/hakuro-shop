d0a68f63c93f21a95db86df6f7759619ac415721
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2024-04-28 12:25:42 +0300

    Create base pages

---
+++ frontend/src/pages/not-found-page.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return (
+        <div className="flex justify-center">
+            <h1 className="mt-80 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">404 PAGE NOT FOUND</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
