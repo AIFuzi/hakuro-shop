2113ed9895546abda570dac614ee5fbed1ee013b
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2024-04-28 12:25:18 +0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
 import {ThemeProvider} from "@/components/ui/theme-provider.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import RegisterPage from "@/pages/register-page.tsx";
+import LoginPage from "@/pages/login-page.tsx";
+import HomePage from "@/pages/home-page.tsx";
+import NotFoundPage from "@/pages/not-found-page.tsx";
+import Navbar from "@/components/navbar.tsx";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark">
-        <div>Hello</div>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path="/signup" element={<RegisterPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/*" element={<NotFoundPage/>}/>
+            </Routes>
+        </BrowserRouter>
     </ThemeProvider>
   )
 }
