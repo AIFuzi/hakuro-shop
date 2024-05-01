11fd7834f7697ec7716e4824a6b34f8be46335cb
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2024-05-02 02:08:45 +0500

    Container navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -7,25 +7,27 @@ const Navbar = observer(() => {
     const navigate = useNavigate()
 
     return (
-        <div className="sticky top-0">
-            <div className="border-b-2 border-zinc-900 h-14 flex items-center justify-between backdrop-blur-sm bg-zinc-950/30">
-                <div>
-                    <Button variant="link" onClick={() => navigate('/')}>
-                        <img alt="" src={logo} width="200" className="hover:contrast-0"/>
-                    </Button>
-                </div>
-                <div>
-                    <ul className="flex flex-row justify-between space-x-5">
-                        <li>Shirts</li>
-                        <li>Shirts</li>
-                        <li>Shirts</li>
-                        <li>Shirts</li>
-                        <li>Shirts</li>
-                    </ul>
-                </div>
-                <div className="mr-5">
-                    <Button variant="ghost" onClick={() => navigate('/signup')} className="mr-2">Sign Up</Button>
-                    <Button variant="ghost" onClick={() => navigate('/login')}>Login</Button>
+        <div className="sticky top-0 border-b-2 border-zinc-900">
+            <div className="container">
+                <div className="h-14 flex items-center justify-between backdrop-blur-sm bg-zinc-950/70">
+                    <div>
+                        <Button variant="link" onClick={() => navigate('/')}>
+                            <img alt="" src={logo} width="200" className="hover:contrast-0"/>
+                        </Button>
+                    </div>
+                    <div>
+                        <ul className="flex flex-row justify-between">
+                            <li><Button variant="link">Shirt</Button></li>
+                            <li><Button variant="link">Shirt</Button></li>
+                            <li><Button variant="link">Shirt</Button></li>
+                            <li><Button variant="link">Shirt</Button></li>
+                            <li><Button variant="link">Shirt</Button></li>
+                        </ul>
+                    </div>
+                    <div className="mr-5">
+                        <Button variant="ghost" onClick={() => navigate('/signup')} className="mr-2">Sign Up</Button>
+                        <Button variant="ghost" onClick={() => navigate('/login')}>Login</Button>
+                    </div>
                 </div>
             </div>
         </div>
