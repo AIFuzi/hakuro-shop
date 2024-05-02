9a9df72d488b124708e94cfeb1c6bb92c07d1ff5
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2024-05-02 16:43:10 +0300

    Test product dialog

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,7 +1,7 @@
 const ProductCard = () => {
     return (
         <div className="flex flex-col cursor-pointer">
-            <img alt="" className="object-contain w-full rounded-2xl" src="http://localhost:4000/6d87337d-d03b-444e-aa47-e2b16aa5b45a.png"/>
+            <img alt="" className="object-contain w-full rounded-2xl" src={import.meta.env.VITE_API_PRODUCT_IMAGE}/>
             <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">Product name</h2>
             <h3 className="text-red-500">$400</h3>
         </div>
--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -1,9 +1,21 @@
 import ProductCard from "@/components/ProductCard.tsx";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription, DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {useState} from "react";
 
 const HomePage = () => {
+    const [open, setOpen] = useState(false)
+
     return (
         <div className="container mt-10">
-            <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight">Type name</h1>
+            <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight">Type name</h1>
             <div className="grid grid-cols-3 place-items-center gap-10">
                 <ProductCard/>
                 <ProductCard/>
@@ -12,6 +24,21 @@ const HomePage = () => {
                 <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
+                <Button onClick={() => setOpen(true)}>Open</Button>
+                <Dialog open={open}>
+                    <DialogTrigger></DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Bob</DialogTitle>
+                            <DialogDescription>
+                                Test bob ac
+                            </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                            <Button onClick={() => setOpen(false)}>Close</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
             </div>
         </div>
     );
